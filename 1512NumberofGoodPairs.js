a0274485f2160a482514f424e7a8032e781bb254
dhfernando <hdilshan.fernando11@gmail.com>
2021-03-25 21:45:38 +0530

    1512 Number of Good Pairs

---
+++ 1512NumberofGoodPairs.js
@@ -0,0 +1,13 @@
+const nums = [1,2,3]
+
+let count = 0;
+
+for (let i = 0; i < nums.length; i++) {
+    for (let j = 1+i; j < nums.length; j++) {
+        // console.log(j)
+
+        if(nums[i] === nums[j]) count++;
+    }
+}
+
+console.log(count)
