66ad3086d6d58522a72ef2c4a26a2b223c2e5235
dhfernando <hdilshan.fernando11@gmail.com>
2021-03-25 20:08:17 +0530

    1480 solved

--- 1480RunningSumof1dArray.js
+++ 1480RunningSumof1dArray.js
@@ -1,1 +1,15 @@
-console.log("hey")
+const nums = [1,2,3,4]
+
+let sumArray = []
+
+for (let i = 0; i < nums.length; i++) {
+    const element = nums[i];
+    if(i===0){
+        sumArray.push(nums[i])
+    }else{
+        sumArray = [ ...sumArray , nums[i] + sumArray[sumArray.length - 1] ]
+    }
+    
+}
+
+console.log(sumArray)
