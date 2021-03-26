247ec044de6e810fb9c433b3833cdc438986d6ea
dhfernando <hdilshan.fernando11@gmail.com>
2021-03-26 21:29:03 +0530

    1365 How Many Numbers Are Smaller Than the Current Number

---
+++ 1365HowManyNumbersAreSmallerThantheCurrentNumber.js
@@ -0,0 +1,15 @@
+const nums = [7,7,7,7]
+
+let output = []
+
+for (let i = 0; i < nums.length; i++) { 
+    let count = 0;
+    nums.forEach(el => {
+        if(nums[i]>el){
+            count++;
+        }
+    });
+    output = [...output, count]
+}
+
+console.log(output)
