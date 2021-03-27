36492dc55e467aa9f4eb58a6c0facda6f4b6f784
dhfernando <hdilshan.fernando11@gmail.com>
2021-03-27 23:06:40 +0530

    1221 Split A String In Balanced Strings

---
+++ 1221SplitAStringInBalancedStrings.js
@@ -0,0 +1,17 @@
+const s = "RLRRLLRLRL"
+
+let res = 0;
+
+var r_counetr = 0 ;
+var l_counetr = 0 ;
+
+for (let i = 0; i < s.length; i++) {
+    const el = s[i];
+    if(el === 'R') {r_counetr++; console.log(r_counetr);}
+    else {l_counetr++}
+    
+    if(r_counetr === l_counetr) {res++}
+
+}
+
+console.log(res)
