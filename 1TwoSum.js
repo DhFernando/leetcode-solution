bd0d195d6a81e48fbef5bfd75dfb37a1b520d467
dhfernando <hdilshan.fernando11@gmail.com>
2021-03-28 22:21:47 +0530

    1 Two Sum

---
+++ 1TwoSum.js
@@ -0,0 +1,15 @@
+const nums = [2,7,11,15]
+const target = 9
+
+let res = []
+
+for (let i = 0; i < nums.length; i++) { 
+    for (let j = 0; j < nums.length; j++) {    
+        if(nums[i] + nums[j] === target && i != j){ 
+            res = [...res , i , j]
+            return res;
+        }
+    }
+}
+
+console.log("res")
