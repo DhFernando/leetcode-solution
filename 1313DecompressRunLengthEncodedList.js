675c66dae24d01dcfb0e95748f9767bc9b908560
dhfernando <hdilshan.fernando11@gmail.com>
2021-03-31 16:50:55 +0530

    return out

---
+++ 1313DecompressRunLengthEncodedList.js
@@ -0,0 +1,7 @@
+const nums = [1,2,3,4]
+
+let out = []
+    for (let i = 0; i , i < nums.length - 1; i += 2) {
+        out = [...out, ...Array(nums[i]).fill(nums[i + 1])]
+    }
+    return out 
--- 1342NumberStepsToReduceNumberToZero.js
+++ 1342NumberStepsToReduceNumberToZero.js
@@ -2,15 +2,13 @@ let num = 3
 
 let count = 0
 while(num != 0){
-     
-        if(num%2 === 0){
-            num = num/2
-            count++
-        }else{
-            num--
-            count++
-        }
-    
+    if(num%2 === 0){
+        num = num/2
+        
+    }else{
+        num-- 
+    }
+    count++
 }
 
-console.log(count)
+return count
