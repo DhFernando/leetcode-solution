e1af3dce614de54d42f76536223ca3213593c8b9
dhfernando <hdilshan.fernando11@gmail.com>
2021-04-15 23:06:24 +0530

    1822SignProductArray

---
+++ 1822SignProductArray.js
@@ -0,0 +1,12 @@
+const nums = [1,5,0,2,-3]
+
+let temp = 1
+nums.forEach(el => {
+    temp = temp*el
+    console.log(temp)
+});
+
+console.log(nums.indexOf(0)) 
+if( nums.indexOf(0) === 1  ){ return 0 }
+else if( temp > 1 ){ return 1 }
+else if( temp < 1 ){ return -1 }
